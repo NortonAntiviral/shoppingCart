926bc3a56177cee3ba29d2cdc8cb072b01b4f714
NortonAntiviral <officialantiviral@gmail.com>
2019-01-31 01:00:31 -0500

    print add to cart

--- js/app.js
+++ js/app.js
@@ -1,8 +1,33 @@
 // VARIABLES
-const 
+const courses = document.querySelector('#courses-list');
 
 // LISTENERS
+
+loadEventListeneers();
+
+function loadEventListeneers() {
+    // Whe a new course is added
+    courses.addEventListener('click',buyCourse);
+}
 
 
 
 // FUNCTIONS
+
+function buyCourse(e) {
+    e.preventDefault();
+    // Use delegation to find the couse that was added
+    if(e.target.classList.contains('add-to-cart')){
+        //read the course values
+        // console.log(e.target.parentElement);
+        const course = e.target.parentElement.parentElement;
+
+        //read values
+        getCourseInfo(course);
+    };
+}
+
+// Reads the HTML info of the selected course
+function getCourseInfo(course) {
+    console.log.(course);
+}
